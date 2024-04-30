0cd560cc402c52b830a24a4ed2eb84e4ea2ca7b1
Lewis Alva <lewis@j1.support>
2024-04-29 23:02:15 -0700

    fix window reference

--- src/web/services/client.ts
+++ src/web/services/client.ts
@@ -2,8 +2,8 @@ import { treaty } from '@elysiajs/eden';
 
 import type { API } from '~/server/routes';
 
-export const client = treaty<API>(window.location.origin);
-export const authenticatedClient = treaty<API>(window.location.origin, {
+export const client = treaty<API>(global.window?.location?.origin);
+export const authenticatedClient = treaty<API>(global.window?.location?.origin, {
   fetch: { credentials: 'include' },
 });
 
